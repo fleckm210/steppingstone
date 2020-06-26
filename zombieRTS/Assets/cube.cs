6e1a61630b15e1e63ca1463e3153bcb5f47ae16f
fleckm210 <fleckm210@gmail.com>
2020-06-26 17:43:54 -0600

    worked on outline

---
+++ zombieRTS/Assets/cube.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    public Transform tf;
+    public Rigidbody rb;
+    bool onground = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && onground)
+        {
+            rb.AddForce(0, 30, 0);
+            onground = false;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            tf.position = new Vector3(tf.position.x - 0.3f, tf.position.y, tf.position.z);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "Cube (1)")
+        {
+            onground = true;
+        }
+    }
+}
