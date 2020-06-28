6a8895fb9e14a6179e6764589804b0224ea23d36
fleckm210 <fleckm210@gmail.com>
2020-06-28 09:53:31 -0600

    worked on some RTS components

---
+++ zombieRTS/Assets/mousecontroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity;
+
+public class mousecontroller : MonoBehaviour
+{
+    public GameObject Button;
+    private Transform btf;
+    public Transform tf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btf = Button.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tf.position = Input.mousePosition;
+    }
+
+    public void Click()
+    {
+        btf.position = tf.position;
+    }
+}
