3faf192d982b9118448592ea15396eb33a76f2ca
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-07-07 21:31:31 -0500

    changed the mouse input to not use a button

---
+++ zombieRTS/Assets/player2Controller.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2Controller : MonoBehaviour
+{
+    public GameObject camObj;
+    Camera mainCam;
+    public Transform playerTF;
+    Vector3 playerScreenPos;
+    Vector3 newPosition; //where the player is going next
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = camObj.GetComponent<Camera>();
+
+        //Debug.Log(mainCam.WorldToScreenPoint(new Vector3(0, 0, 0)));
+
+        playerScreenPos = mainCam.WorldToScreenPoint(playerTF.position);
+
+        //Make the new position start as the current player position
+        newPosition = playerTF.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Left click is held down
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log(Input.mousePosition.x);
+            Debug.Log(playerScreenPos.x);
+
+            //tell the player where to go next
+
+            //covert mouse position into world postion
+            newPosition = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+            //have the player move to new position
+
+
+        }
+    }//END OF Update
+
+    private void FixedUpdate()
+    {
+
+        if (playerTF.position.x < newPosition.x - 4)
+        {
+            playerTF.position = new Vector3(playerTF.position.x + 6, playerTF.position.y, playerTF.position.z);
+        }
+        else if (playerTF.position.x > newPosition.x + 4)
+        {
+            playerTF.position = new Vector3(playerTF.position.x - 6, playerTF.position.y, playerTF.position.z);
+        }
+
+        if (playerTF.position.y < newPosition.y - 4)
+        {
+            playerTF.position = new Vector3(playerTF.position.x, playerTF.position.y + 6, playerTF.position.z);
+        }
+        else if (playerTF.position.y > newPosition.y + 4)
+        {
+            playerTF.position = new Vector3(playerTF.position.x, playerTF.position.y - 6, playerTF.position.z);
+        }
+    }//END OF FixedUpdate
+}
--- zombieRTS/Assets/zombiecontroller.cs
+++ zombieRTS/Assets/zombiecontroller.cs
@@ -6,32 +6,34 @@ public class zombiecontroller : MonoBehaviour
 {
     public GameObject player;
     public Transform tf;
+    float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 2;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (tf.position.x < player.transform.position.x)
+        //Debug.Log(player.GetComponent<RectTransform>().rect.center.x);
+        if (tf.position.x < player.transform.position.x - 5)
         {
-            tf.position = new Vector3(tf.position.x + 1, tf.position.y, tf.position.z);
+            tf.position = new Vector3(tf.position.x + speed, tf.position.y, tf.position.z);
         }
         else if (tf.position.x > player.transform.position.x)
         {
-            tf.position = new Vector3(tf.position.x - 1, tf.position.y, tf.position.z);
+            tf.position = new Vector3(tf.position.x - speed, tf.position.y, tf.position.z);
         }
 
-        if (tf.position.y < player.transform.position.y)
+        if (tf.position.y < player.transform.position.y - 5)
         {
-            tf.position = new Vector3(tf.position.x, tf.position.y + 1, tf.position.z);
+            tf.position = new Vector3(tf.position.x, tf.position.y + speed, tf.position.z);
         }
         else if (tf.position.y > player.transform.position.y)
         {
-            tf.position = new Vector3(tf.position.x, tf.position.y - 1, tf.position.z);
+            tf.position = new Vector3(tf.position.x, tf.position.y - speed, tf.position.z);
         }
     }
 }
