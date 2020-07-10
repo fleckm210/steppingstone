af603b1eb3601c6ce48245b318b883e90b254578
fleckm210 <fleckm210@gmail.com>
2020-07-09 20:00:16 -0600

    created matrix sim with new io code

---
+++ matrix_sim/matrix_sim/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace matrix_sim
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Random m = new Random();
+
+            while (true)
+            {
+                Console.Clear();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.SetCursorPosition(m.Next(0, 80), m.Next(0, 24));
+
+                    Console.Write(m.Next(0, 10));
+                }
+
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.SetCursorPosition(m.Next(0, 5), m.Next(0, 24));
+
+                    Console.Write(m.Next(0, 10));
+                }
+
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.SetCursorPosition(m.Next(35, 40), m.Next(0, 24));
+
+                    Console.Write(m.Next(0, 10));
+                }
+
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.SetCursorPosition(m.Next(70, 75), m.Next(0, 24));
+
+                    Console.Write(m.Next(0, 10));
+                }
+            }
+        }
+    }
+}
